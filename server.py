cbaf4ad87116275801a90b2828b6663c8fd7ef35
yuyitang <yt2820@columbia.edu>
2022-11-13 14:16:37 -0500

    Fixed packages import stuff

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ from multiprocessing import context
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect
+from flask import Flask, request, render_template, g, redirect, Response
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
